023521745038b72ccda586224ef2ffa9a9309ddc
Leonardo de Borba <leonardodeborbadev@protonmail.com>
2024-03-22 10:42:06 -0300

    Add week 6 assignment

---
+++ 1-building-web-applications-in-php/week6-php-arrays/assignment/assignment-guessing-game.php
@@ -0,0 +1,30 @@
+<html lang="en-US">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>Guessing Game for Leonardo de Borba (1d1a704f)</title>
+  </head>
+  <body>
+    <h1>Welcome to my guessing game</h1>
+  <?php
+    $correct_answer = 39;
+    if (!isset($_GET['guess'])) {
+      echo "Missing guess parameter";
+    }
+    else if (strlen($_GET['guess']) < 1) {
+      echo "Your guess is too short";
+    }
+    else if (!is_numeric($_GET['guess'])) {
+      echo "Your guess is not a number";
+    }
+    else if ($_GET['guess'] < $correct_answer) {
+      echo "Your guess is too low";
+    }
+    else if ($_GET['guess'] > $correct_answer) {
+      echo "Your guess is too high";
+    } else {
+      echo "Congratulations - You are right";
+    }
+  ?>
+  </body>
+</html>
